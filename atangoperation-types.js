ab6277081fa6b93be2f6ab5f13fb57d8a1511570
Atang Makara <atangmakara16@gmail.com>
2025-06-02 13:52:37 +0200

    Add files via upload

---
+++ atangoperation-types.js
@@ -0,0 +1,19 @@
+// TODO: Create a variable called number1 and store any number in it
+let number1= 45;
+
+// TODO: Create a variable called number2 and store a different number in it
+let number2= 60;
+
+// TODO: Convert number1 to a string and store it in a new variable
+let stringNumber = String(number1); 
+console.log(typeof stringNumber);   
+
+// TODO: Add number1 and number2 and store the result in a variable called sum
+const sum = number1 + number2;
+console.log(`The sum of num1 and num2 is  ${sum}`);
+
+// TODO: Multiply number1 and number2 and store the result in a variable called product
+const product = number1 * number2;
+console.log(`The product of num1 and num2 is  ${product}`);
+
+// TODO: Print all results to the console
