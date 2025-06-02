7e2f49fe76c05285649c84da843de3088a077de3
Atang Makara <atangmakara16@gmail.com>
2025-06-02 13:54:47 +0200

    Add files via upload

---
+++ atangarray.js
@@ -0,0 +1,20 @@
+// TODO: Create an empty array called colors
+const colors=[];
+
+// TODO: Add three colors to the array using push()
+colors.push("red", "pink", "black");
+
+// TODO: Create another array called numbers with five numbers
+const numbers= [1, 2, 3, 4, 5];
+
+// TODO: Remove the last color from the colors array
+colors.pop();
+
+console.log(colors); 
+
+// TODO: Add a new color to the beginning of the colors array
+colors.unshift("blue");
+
+console.log(colors);
+// TODO: Print the length of both arrays
+console.log("Length of colors array:", colors.length);
