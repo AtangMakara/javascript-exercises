34b39634907dbbc5693c3477ca10cde8aca45c4a
Atang Makara <atangmakara16@gmail.com>
2025-06-02 13:56:40 +0200

    Add files via upload

---
+++ conditionals.js
@@ -0,0 +1,25 @@
+// TODO: Create a variable called score and give it a value between 0 and 100
+let score = 65
+
+// TODO: Create a variable called passingGrade and set it to 60
+let passingGrade = 60 
+
+// TODO: Write an if statement to check if score is greater than or equal to passingGrade
+if( score >= passingGrade){
+    console.log("You have passed");
+}
+
+// TODO: Add an else statement for when the score is below passingGrade
+else{
+    console.log("Try again")
+}
+
+// TODO: Print "Passed" or "Failed" based on the score
+if(score >= passingGrade){
+    console.log("passed")
+}
+
+// TODO: Add another condition to check if the score is perfect (100)
+if(score === 100){
+    console.log("Perfect score!");
+}
