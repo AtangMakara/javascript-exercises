4a682f2a9e201559e1bb83aad28a47ff45e6266b
Atang Makara <atangmakara16@gmail.com>
2025-06-02 14:00:25 +0200

    Add files via upload

---
+++ atangloops.js
@@ -0,0 +1,23 @@
+// TODO: Create a function called greet that takes a name parameter
+function greet(name){
+    console.log(`Hello ${name}`);
+} 
+
+// TODO: Make the greet function return "Hello, [name]!"
+greet("Atang");
+
+// TODO: Create a function called add that takes two parameters
+function add(a, b){
+    console.log(`sum of ${a} and ${b} is: ${a+b}`);
+}
+
+// TODO: Make the add function return the sum of both parameters
+add(3,4);
+
+// TODO: Call both functions and store their results in variables
+let greeting = greet("Atang");
+let sumResult = add(3,4);
+
+// TODO: Print the results to the console
+console.log(greeting);
+console.log(sumResult);
