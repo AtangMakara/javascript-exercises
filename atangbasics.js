05a168587973bfea42af0ba7facd22a0d2e80dfa
Atang Makara <atangmakara16@gmail.com>
2025-06-02 12:41:49 +0200

    Update atangbasics.js

---
+++ atangbasics.js
@@ -0,0 +1,18 @@
+// TODO: Create a variable called firstName and store your first name in it
+const firstName = "Atang";
+
+// TODO: Create a variable called lastName and store your last name in it
+const lastName = "Makara";
+
+// TODO: Create a variable called age using let (you'll modify this later)
+let age = 19;
+
+// TODO: Create a fullName variable that combines firstName and lastName
+const fullName = firstName + " " + lastName;
+
+// TODO: Print a greeting to the console using these variables
+console.log(`Hello, my name is ${fullName} and I am ${19} years old.`);
+
+// TODO: Try changing the age variable to a different value
+age = 30;
+console.log(" 11 years later! Now I'm " + age + " years old.");
